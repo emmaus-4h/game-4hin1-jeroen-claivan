28a3fa2c304590b3281ab28ffb90770dea40d49a
Jeroen420 <93194709+Jeroen420@users.noreply.github.com>
2022-03-29 11:20:14 +0000

    bal

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
-var spelerX = 600; // x-positie van speler
-var spelerY = 600; // y-positie van speler
+var spelerX = 640; // x-positie van speler
+var spelerY = 360; // y-positie van speler
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -58,9 +58,10 @@ var tekenAlles = function () {
 
   // speler
   fill("white");
-  rect(spelerX - 25, spelerY - 25, 50, 50);
+  ellipse(spelerX, spelerY , 50, 50);
   fill("black");
-  ellipse(spelerX, spelerY, 10, 10);
+  ellipse(spelerX, spelerY , 30, 30);
+
 
   // punten en health
 
@@ -89,7 +90,7 @@ function setup() {
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  background('pink');
 }
 
 /**
